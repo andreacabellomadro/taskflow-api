088469507feaea3fac2bb4920ebb354911a63a3b
andrea <acabello@itpfp.com>
2026-09-04 11:27:09 +0200

    chore: initial project setup with spring initializr

---
+++ src/test/java/com/miportfolio/taskflow_api/TaskflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miportfolio.taskflow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
